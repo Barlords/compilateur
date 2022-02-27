39b5b0b986ebc8a12b14c722da1a62d551a643ed
Barlords <basile.pulin@gmail.com>
2022-02-27 03:49:07 +0100

    patch

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 ## affectation, print
 s1 = 'x=4; x=x+3; print(x);'
+TEST : OK
 
 ## affectation élargie, affectation 
 s2 = 'x=9; x+=4; x++; print(x);'
+TEST : OK
 
 ## while, for
 s3 = 'x=4; while(x<30){x=x+3;print(x);};for(i=0 ; i<4 ; i=i+1){print(i*i) ;};'
+TEST : OK
+
+## fonctions void avec paramètres
+s4='func toto(a, b){print(a+b) ;}; toto(3, 5) ;'
+
+## fonctions value avec paramètres et return explicite
+s5='func toto(a, b){c=a+b ;return c ;} toto(3, 5) ;'
+
+## fonctions value avec paramètres et return implicite
+s5='func toto(a, b){c=a+b ; toto=c ;} toto(3, 5) ;'
+
+## fonctions value avec paramètres et return coupe circuit
+s6='func toto(a, b){c=a+b ;return c ; print(666) ;} x=toto(3, 5) ; print(x) ;'
+
+## fonctions value avec paramètres, return coupe circuit et scope des variables
+s7='func toto(a, b){if(a==0) return b ; c=toto(a-1, b-1) ;return c ; print(666) ;} x=toto(3, 5) ;
+print(x) ;'
--- calcBase.py
+++ calcBase.py
@@ -12,37 +12,37 @@ precedence = (
     ('nonassoc', 'IS_SUPP', 'IS_SUPP_0R_EQUALS', 'IS_INF', 'IS_INF_OR_EQUALS', 'IS_EQUALS'),
     ('left', 'PLUS', 'MINUS'),
     ('left', 'TIMES', 'DIVIDE'),
-    )
+)
 
 reserved = {
-    'func' : 'FUNCTION',
-    'print' : 'PRINT',
-    'empty' : 'EMPTY',
-    'while' : 'WHILE',
-    'for' : 'FOR',
-    'if' : 'IF',
-    'else' : 'ELSE',
-    'void' : 'VOID',
-    'null' : 'NULL',
-    'return' : 'RETURN',
+    'func': 'FUNCTION',
+    'print': 'PRINT',
+    'empty': 'EMPTY',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'if': 'IF',
+    'else': 'ELSE',
+    'void': 'VOID',
+    'null': 'NULL',
+    'return': 'RETURN',
 }
 
 names = dict()
 functions = dict()
 
 tokens = [
-    'NUMBER', 'NAME',
-    'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'EQUALS',
-    'PLUS_PLUS', 'MINUS_MINUS',
-    'LPAREN', 'RPAREN', 'LACO', 'RACO', 'QUOT', 'COMMA',
-    'AND', 'OR', 'TRUE', 'FALSE',
-    'SEMICOLON', 'SPACE',
-    'INCR', 'DECR',
-    'MINUS_EQUALS', 'PLUS_EQUALS', 'TIMES_EQUALS', 'DIVIDE_EQUALS',
-    'IS_SUPP', 'IS_SUPP_0R_EQUALS', 'IS_INF', 'IS_INF_OR_EQUALS', 'IS_EQUALS',
-    'LCOMMENT', 'RCOMMENT',
+             'NUMBER', 'NAME',
+             'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'EQUALS',
+             'PLUS_PLUS', 'MINUS_MINUS',
+             'LPAREN', 'RPAREN', 'LACO', 'RACO', 'QUOT', 'COMMA',
+             'AND', 'OR', 'TRUE', 'FALSE',
+             'SEMICOLON', 'SPACE',
+             'INCR', 'DECR',
+             'MINUS_EQUALS', 'PLUS_EQUALS', 'TIMES_EQUALS', 'DIVIDE_EQUALS',
+             'IS_SUPP', 'IS_SUPP_0R_EQUALS', 'IS_INF', 'IS_INF_OR_EQUALS', 'IS_EQUALS',
+             'LCOMMENT', 'RCOMMENT',
 
-] + list(reserved.values())
+         ] + list(reserved.values())
 
 # Tokens
 t_PLUS = r'\+'
@@ -76,16 +76,17 @@ t_LCOMMENT = r'\/\*'
 t_RCOMMENT = r'\*\/'
 t_SPACE = r'\s'
 
+
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, 'NAME')
     return t
-
 
 
 # Ignored characters
@@ -95,6 +96,7 @@ t_ignore = " \t"
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
+
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
@@ -106,6 +108,7 @@ import ply.lex as lex
 
 lex.lex()
 
+
 def p_start(p):
     '''START : bloc '''
     p[0] = ('START', p[1])
@@ -113,9 +116,10 @@ def p_start(p):
     printTreeGraph(p[0])
     try:
         evalInst(p[1])
-    except KeyError as ke:
-        print("Variable non initialisée :")
-        print(ke)
+    except Exception() as e:
+        print("Une exception est survenue :")
+        print(e)
+
 
 def p_bloc(p):
     '''bloc : bloc statement SEMICOLON
@@ -131,6 +135,7 @@ def p_bloc(p):
     elif p[2] == '/*' and p[4] == '*/':
         p[0] = ('bloc', p[1], 'empty')
 
+
 def p_statement_print(p):
     '''statement    : PRINT LPAREN expression RPAREN
                     | PRINT LPAREN QUOT expression QUOT RPAREN'''
@@ -139,99 +144,120 @@ def p_statement_print(p):
     elif len(p) == 7:
         p[0] = ('print_str', p[4])
 
+
 def p_statement_comment(p):
     '''comment  : NAME
                 | NAME SPACE comment'''
+
 
 def p_statement_assign(p):
     'statement : NAME EQUALS expression'
     p[0] = ('assign', p[1], p[3])
 
+
 def p_statement_increment_var(p):
-    '''statement : NAME INCR
-                | NAME DECR'''
+    '''statement    : NAME INCR
+                    | NAME DECR'''
     p[0] = (p[2], p[1])
 
+
 def p_statement_modif_var(p):
-    '''statement : NAME PLUS_EQUALS expression
-                | NAME MINUS_EQUALS expression
-                | NAME TIMES_EQUALS expression
-                | NAME DIVIDE_EQUALS expression'''
+    '''statement    : NAME PLUS_EQUALS expression
+                    | NAME MINUS_EQUALS expression
+                    | NAME TIMES_EQUALS expression
+                    | NAME DIVIDE_EQUALS expression'''
     p[0] = (p[2], p[1], p[3])
+
 
 def p_expression_opperation(p):
-    '''expression : expression MINUS expression
-				| expression PLUS expression
-				| expression TIMES expression
-				| expression DIVIDE expression'''
+    '''expression   : expression MINUS expression
+			        | expression PLUS expression
+				    | expression TIMES expression
+				    | expression DIVIDE expression'''
     p[0] = (p[2], p[1], p[3])
 
+
 def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
+    'expression     : LPAREN expression RPAREN'
     p[0] = p[2]
 
+
 def p_expression_number(p):
-    '''expression : NUMBER
-                | NAME'''
+    '''expression   : NUMBER
+                    | NAME'''
     p[0] = p[1]
 
+
 def p_expression_bool(p):
-    '''expression : TRUE
-                | FALSE'''
+    '''expression   : TRUE
+                    | FALSE'''
     if p[1] == 'T':
         p[0] = True
     elif p[1] == 'F':
         p[0] = False
 
+
 def p_expression_bool_compare(p):
-    '''expression : expression IS_SUPP expression
-                | expression IS_SUPP_0R_EQUALS expression
-                | expression IS_INF expression
-                | expression IS_INF_OR_EQUALS expression
-                | expression IS_EQUALS expression
-                | expression AND expression
-                | expression OR expression'''
+    '''expression   : expression IS_SUPP expression
+                    | expression IS_SUPP_0R_EQUALS expression
+                    | expression IS_INF expression
+                    | expression IS_INF_OR_EQUALS expression
+                    | expression IS_EQUALS expression
+                    | expression AND expression
+                    | expression OR expression'''
     p[0] = (p[2], p[1], p[3])
 
+
 def p_statement_if(p):
-    '''statement : IF LPAREN expression RPAREN LACO bloc RACO
-            | IF LPAREN expression RPAREN LACO bloc RACO ELSE LACO bloc RACO '''
+    '''statement    : IF LPAREN expression RPAREN LACO bloc RACO
+                    | IF LPAREN expression RPAREN LACO bloc RACO ELSE LACO bloc RACO '''
     if len(p) == 8:
         p[0] = ('if', p[3], p[6])
     else:
         p[0] = ('if', p[3], p[6], ('else', p[10]))
 
+
 def p_statement_while(p):
-    '''statement : WHILE LPAREN expression RPAREN LACO bloc RACO'''
+    '''statement    : WHILE LPAREN expression RPAREN LACO bloc RACO'''
     p[0] = ('while', p[3], p[6])
 
+
 def p_statement_for(p):
-    '''statement : FOR LPAREN statement SEMICOLON expression SEMICOLON statement RPAREN LACO bloc RACO'''
+    '''statement    : FOR LPAREN statement SEMICOLON expression SEMICOLON statement RPAREN LACO bloc RACO'''
     p[0] = ('for', p[3], p[5], p[7], p[10])
 
+
 def p_args(p):
-    '''args : expression
-            | expression COMMA args'''
+    '''args     : expression
+                | expression COMMA args'''
+    args = list()
     if len(p) == 2:
-        p[0] = ('param', p[1])
-    else:
-        p[0] = ('param', p[3], p[1])
+        p[0] = p[1]
+    elif len(p) == 4:
+        p[0] = (p[1], p[3])
+
 
 def p_statement_declare_func(p):
     '''statement    : FUNCTION NAME LPAREN RPAREN LACO bloc RACO
                     | FUNCTION NAME LPAREN args RPAREN LACO bloc RACO'''
     if len(p) == 8:
-        p[0] = ('func', (p[2], ('param', 'empty'), p[6]))
-        functions[p[2]] = p[6]
+        p[0] = ('func', p[2], ('param', 'empty'), p[6])
     elif len(p) == 9:
-        p[0] = ('func', (p[2], p[4], p[7]))
+        p[0] = ('func', p[2], ('param', p[4]), p[7])
+
 
 def p_statement_call_func(p):
     '''statement    : NAME LPAREN RPAREN
                     | NAME LPAREN args RPAREN'''
+    if len(p) == 4:
+        p[0] = ('call', p[1])
+    elif len(p) == 5:
+        p[0] = ('call', p[1], ('param', p[3]))
+
 
 def p_error(p):
     print("Syntax error at '%s'" % p.value)
+
 
 import ply.yacc as yacc
 
@@ -239,11 +265,9 @@ yacc.yacc()
 
 
 def evalExpr(t):
-    #print("evalExpr", t)
+    # print("evalExpr", t)
     if type(t) is int: return t
     if type(t) is str: return names[t]
-    if t[0] == 'call':
-        return evalExpr(t[3])
     if t[0] == '+':
         return evalExpr(t[1]) + evalExpr(t[2])
     if t[0] == '-':
@@ -252,7 +276,6 @@ def evalExpr(t):
         return evalExpr(t[1]) * evalExpr(t[2])
     if t[0] == '/':
         return evalExpr(t[1]) / evalExpr(t[2])
-
 
     if t[0] == '>':
         return evalExpr(t[1]) > evalExpr(t[2])
@@ -270,11 +293,11 @@ def evalExpr(t):
     if t[0] == '|':
         return evalExpr(t[1]) or evalExpr(t[2])
 
-
     return 'UNK'
 
+
 def evalInst(t):
-    #print("evalInst",t)
+    #print("evalInst", t)
     if t == 'empty':
         return
     if t[0] == 'print':
@@ -284,8 +307,8 @@ def evalInst(t):
     if t[0] == 'bloc':
         evalInst(t[1])
         evalInst(t[2])
-        #pile.push(p[1])
-        #pile.push(p[2])
+        # pile.push(p[1])
+        # pile.push(p[2])
 
     if t[0] == 'if':
         if evalExpr(t[1]):
@@ -302,7 +325,38 @@ def evalInst(t):
             evalInst(t[3])
 
     if t[0] == 'func':
-        functions[t[1]] = t[2]
+        func = dict()
+        if t[2][1] != 'empty':
+            params = list()
+            p = t[2][1]
+            while p is tuple() and len(p) == 2:
+                params.append(p[0])
+                p = p[1]
+            if p is not tuple():
+                params.append(p)
+            func['params'] = params
+        func['content'] = t[3]
+        functions[t[1]] = func
+
+    if t[0] == 'call':
+        func = functions[t[1]]
+        if len(t) == 3:
+            call_params = list()
+            p = t[2][1]
+            while p is tuple() and len(p) == 2:
+                call_params.append(p[0])
+                p = p[1]
+            if p is not tuple:
+                call_params.append(p)
+            if dict(func).get('params', []) is None \
+                    or len(func.get('params', [])) > len(call_params)\
+                    or len(func.get('params', [])) < len(call_params):
+                raise Exception(len(func.get('params', [])), ' argument(s) attendu, ', len(call_params), ' trouvé(s)')
+            for i in range(len(func['params'])):
+                names[func['params'][i]] = call_params[i]
+        print(functions)
+        print(names)
+        evalInst(func['content'])
 
     if t[0] == 'assign':
         names[t[1]] = evalExpr(t[2])
@@ -321,7 +375,6 @@ def evalInst(t):
 
     return t
 
-s = 'func test(testVar) { print("salut");};' \
-    'func test2() { print("sansParam"); };'
 
+s = 'func toto(a, b){print(a+b) ;}; toto(3, 5) ;'
 yacc.parse(s)
